aa2f8c711a7f67121caa671ff72368999b8ef3f2
Facundo Gayoso <facusbrr@gmail.com>
2025-11-05 15:14:40 -0300

    polichan: mat]

--- src/modules/post/application/commands/create-post.command.ts
+++ src/modules/post/application/commands/create-post.command.ts
@@ -4,8 +4,6 @@ export class CreatePostCommand extends Command {
   constructor(
     commandId: string,
     public readonly userId: string,
-    public readonly fullName: string,
-    public readonly username: string,
     public readonly content: string
   ) {
     super(commandId);
