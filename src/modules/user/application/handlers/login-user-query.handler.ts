a740a424181cfce9edfd9e2527d4401a2d4412eb
sr-roboto <marcoslopez123.lu@gmail.com>
2025-11-12 11:49:42 -0300

    refactor: mejorar la legibilidad del manejador de consultas de inicio de sesión

--- src/modules/user/application/handlers/login-user-query.handler.ts
+++ src/modules/user/application/handlers/login-user-query.handler.ts
@@ -5,15 +5,15 @@ import { UserReadRepository } from '../../domain/interfaces/user-read-repository
 import { LoginUserQuery } from '../queries/login-user.query';
 
 export class LoginUserQueryHandler
-  implements QueryHandler<LoginUserQuery, UserReadModel | null> {
-  constructor(private userReadRepository: UserReadRepository) { }
+  implements QueryHandler<LoginUserQuery, UserReadModel | null>
+{
+  constructor(private userReadRepository: UserReadRepository) {}
 
   async handle(query: LoginUserQuery): Promise<any> {
     const user = await this.userReadRepository.finByUserName(query.userName);
     if (!user || user.password !== query.password) {
       return null;
     }
-    // Aquí podrías devolver un JWT o el modelo de usuario según tu necesidad
     return {
       id: user.id,
       firstName: user.firstName,
